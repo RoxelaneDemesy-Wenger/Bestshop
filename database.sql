16852b245964f548c1742160c989f214b5dd4ce5
EmilieMo <mimimilie120@hotmail.com>
2023-01-03 09:59:03 +0100

    modelisation

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS bestshop;
+
+USE bestshop;
+
+CREATE TABLE portable (
+	id_portable	INT NOT NULL AUTO_INCREMENT,
+    marque			VARCHAR(150),
+    couleur			VARCHAR(200),
+    memoire			INT UNSIGNED,
+    prix			INT,
+    photo			VARCHAR(50),
+    taille	        VARCHAR(20),
+    description		MEDIUMTEXT,
+    
+    PRIMARY KEY (id_portable)
+) ENGINE=INNODB CHARACTER SET=utf8;
