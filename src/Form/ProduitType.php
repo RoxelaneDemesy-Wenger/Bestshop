b3e6c01d796c2424ff3ac5e152764043dd88849b
belkasmibilel <116267460+belkasmibilel@users.noreply.github.com>
2023-01-03 12:10:35 +0100

    entity

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('marque')
+            ->add('modele')
+            ->add('couleur')
+            ->add('taille')
+            ->add('prix')
+            ->add('photo')
+            ->add('memoire')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
