490a4d0d36ad106cadd23417274618c35b85d144
Roorox <roxdemwen@gmail.com>
2023-01-04 16:26:46 +0100

    Update AdminType.php

--- src/Form/AdminType.php
+++ src/Form/AdminType.php
@@ -12,11 +12,7 @@ class AdminType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
+            ->add('secret', PasswordType::class)
             ->add('envoyer', SubmitType::class)
         ;
     }
