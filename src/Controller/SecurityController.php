994cdc4e39187c037ac6a5c5305224d69ddb47ae
Roorox <roxdemwen@gmail.com>
2023-01-06 14:30:39 +0100

    securitycontroller user controller autenticator

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,4 +29,65 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/passerAdmin_{id<\d+>}', name: 'app_passer_admin')]
+    public function passerAdmin($id, Request $request, UserRepository $repo){
+        $secret = "123123aA";
+
+        $form = $this->createForm(AdminType::class);
+        $form->handleRequest($request);
+
+        $user = $repo->find($id);
+        //avec le repo/Repository => récupère moi ce user avec cet id
+
+        if(!$user){
+            throw $this->createNotFoundException("Immpossible de trouver l'utilisateur avec l'id : $id");
+        }
+
+        if($form->isSubmitted() && $form->isValid()){
+
+                if($form->get('secret')->getData()== $secret){
+                    $user->setRoles(["ROLE_ADMIN"]);
+                }else{
+                    throw $this->createNotFoundException("Vous n'avez pas le bon code, êtes vous un intru ?");
+                }
+
+            $repo->save($user, 1);
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('security/adminForm.html.twig', [
+            "user" => $user,
+            "formAdmin" => $form->createView()
+        ]);
+    }
+
+    #[Route(path: '/confirmationCompte/{token}', name: 'app_confirmation_token')]
+    public function confirmationCompte($token,UserRepository $repo){
+
+    //récupère l'utilisateur ayant le token de l'url
+
+    $user = $repo->findOneBy(["token" => $token]);
+
+    //on vérifie si un utilisateur est trouvé
+
+    if(!$user){
+
+        $this->addFlash("error", "Aucun utilisateur trouvé");
+
+        return $this->redirectToRoute('app_login');
+    }
+
+    //le token existe est donc l'utilisateur à été vérifié (trouvé dans la db) On enlève le token et on "active" son compte
+    $user->setToken(null)
+         ->setIsValid(true);
+
+    $repo->save($user, 1);
+
+    $this->addFlash("succes", "Félicitation votre compte est bien activé! Bienvenue, connectez-vous.");
+
+    return $this->redirectToRoute('app_login');
+
+    }
 }
